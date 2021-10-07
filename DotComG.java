426d3040cc6a297f42d5b2d1f60cf4e81ebda230
meghrxj <hello.meghrajjj@gmail.com>
2021-10-08 00:01:20 +0530

    Update DotComG.java

---
+++ DotComG.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+
+
+public class DotComG {
+
+    public ArrayList<String> locationCells;
+    private String name;
+
+    public String checkYourself(String userInput) {
+        String result = "miss";
+        int index = locationCells.indexOf(userInput);
+
+        if (index >= 0) {
+            locationCells.remove(index);
+
+            if (locationCells.isEmpty()) {
+                result = "kill";
+            } else {
+                result = "hit";
+            }
+        }
+
+        return result;
+    }
+
+    public void setLocationCells(ArrayList<String> locationCells) {
+        this.locationCells = locationCells;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
